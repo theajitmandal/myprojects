ef2e5e2c1c255a0bc7b43e1c2a9febd6a2a69cc7
ajitdhanukmandal <theajitmandal@gmail.com>
2023-10-11 20:29:19 +0545

    This is second commit

--- js04temperatureconverter/script.js
+++ js04temperatureconverter/script.js
@@ -3,6 +3,15 @@ const calculateTemp = () => {
     // console.log(numberTemp);
 
     const tempSelected = document.getElementById('temp_diff');
-    const valueTemp = temp_diff.options[tempSelected.selectedIndex];
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+    
+    let result;
 
+    if(valueTemp == 'cel'){
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `${result}Celsius`;
+    }else{
+        result = fehToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `${result}Celsius`;
+    }
 }
