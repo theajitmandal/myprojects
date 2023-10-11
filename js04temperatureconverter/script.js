3f87db9b11fb15cf029642a6494968f85780761c
ajitdhanukmandal <theajitmandal@gmail.com>
2023-10-11 20:17:32 +0545

    This is second commit

---
+++ js04temperatureconverter/script.js
@@ -0,0 +1,8 @@
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
+
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex];
+
+}
